7e8783fc4114bfab43c36ad94ca0ab969ec3edee
ZharavinAV <zharavinarkady@gmail.com>
2019-09-21 22:52:54 +0300

    started project

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>There will be something here!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
