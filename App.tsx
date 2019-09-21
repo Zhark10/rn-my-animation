3754366ebdf315256ca4761d4aa7dd459db0e754
ZharavinAV <zharavinarkady@gmail.com>
2019-09-21 23:40:12 +0300

    experimental animation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/Screen/Navigation';
 
 export default function App() {
--- src/Screen/HomeScreen/HomeScreen.tsx
+++ src/Screen/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Animated, Easing } from 'react-native';
+import { styles } from './HomeScreen.style';
 
 type IHomeScreenProps = {}
 type IHomeScreenState = {}
 
 class HomeScreen extends Component<IHomeScreenProps, IHomeScreenState> {
+    spinValue = new Animated.Value(0);
+
+    componentDidMount() {
+        this.spin()
+    }
+    spin() {
+        this.spinValue.setValue(0)
+        Animated.timing(
+            this.spinValue,
+            {
+                toValue: 1,
+                duration: 4000,
+                easing: Easing.linear
+            }
+        ).start(() => this.spin())
+    }
+
     render() {
-        return (
-            <View>
-                <Text>Какой-то текст</Text>
+        const spin = this.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+          })
+          return (
+            <View style={styles.container}>
+              <Animated.Image
+                style={{
+                  width: 200,
+                  height: 200,
+                  transform: [{rotate: spin}] }}
+                  source={{uri: 'https://svgsilh.com/png-512/1781540-ff5722.png'}}
+              />
             </View>
-        )
+          )
     }
 }
 
